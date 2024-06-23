80382ad5a3c826c5c5c91523ddfb74bb9aabaeb1
Marty Trujillo <marty@ilikeemail.com>
2024-06-23 11:51:01 -0400

    Video 005 - implements aria labels.

--- src/app/api-indicator/api-indicator.component.ts
+++ src/app/api-indicator/api-indicator.component.ts
@@ -1,12 +1,16 @@
 import { Component, input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-api-indicator',
   standalone: true,
-  imports: [],
+  imports: [ MatIconModule ],
   templateUrl: './api-indicator.component.html',
   styleUrl: './api-indicator.component.scss'
 })
 export class ApiIndicatorComponent {
-  title = input.required();
+  title = input.required<string>();
+  hasApi = input.required<boolean>();
+  positiveAriaLabel = input.required<string>();
+  negativeAriaLabel = input.required<string>();
 }
